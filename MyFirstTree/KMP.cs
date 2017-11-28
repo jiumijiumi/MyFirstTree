04596b479629b6ff7bc9fc12ff55c94d40b300d1
unknown <1907421040@qq.com>
2017-11-28 14:58:54 +0800

    KMP初次尝试

---
+++ MyFirstTree/KMP.cs
@@ -0,0 +1,73 @@
+﻿using System.Data.SqlTypes;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace MyFirstTree
+{
+    public class KMP
+    {
+        //尝试理解KMP算法https://www.cnblogs.com/tangzhengyue/p/4315393.html
+        //获取next数组
+        public static void GetNext(string keyWord,ref int[]next)
+        {
+            if (string.IsNullOrEmpty(keyWord))
+                return;
+           // int[] next = new int[keyWord.Length];
+            next[0] = -1;//当k为-1的时候，next[j+1]应该等于0，把J退回到首位
+            int k = -1;//假设next[j]=-1
+            int j = 0;//keyWord中的指针
+            while (j < keyWord.Length - 1)//根据已知的前j位，推测j+1位
+            {
+                if (k == -1 || keyWord[j] == next[k])
+                {
+                    if (keyWord[j + 1] == keyWord[k + 1])
+                    {
+                        j++;
+                        k++;
+                        next[j] = next[k];
+                    }
+                    else
+                    {
+                        next[j++] = k++;
+                    }
+                }
+                else
+                {
+                    k = next[k];
+                }
+            }
+        }
+        /// <summary>
+        /// source要搜索的字符串，keyWord关键词
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="keyWord"></param>
+        /// <returns>keyWord在字符串中的开始位置</returns>
+        public static bool KMPSearch(string source, string keyWord)
+        {
+            if (string.IsNullOrEmpty(keyWord) || string.IsNullOrEmpty(source))
+                return false;
+            bool boolToReturn=false;
+            int i = 0, j = 0;
+            var next = new int[keyWord.Length];
+            GetNext(keyWord,ref next);
+            while (i < source.Length && j < keyWord.Length)
+            {
+                if (j == -1 || source[i] == keyWord[j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    j = next[j];
+                }
+            }
+            if (j == keyWord.Length)
+            {
+                boolToReturn = true;
+            }
+            return boolToReturn;
+        }
+    }
+}
--- MyFirstTree/Program.cs
+++ MyFirstTree/Program.cs
@@ -12,33 +12,32 @@ using SensitiveWordFilter;
 namespace MyFirstTree
 {
     class Program
-    {    //对于生成的树中的敏感词也可以进行按照拼音排序啊啊啊啊啊啊，怎么就只想到了前面呢  然排序首要还是要实现搜索
-        //二分搜索树
+    {   //对按照拼音排序没有太大的兴趣了
+        //二分搜索树  KMP算法，Trie树，AC自动机，BFS广度优先搜索
         //目标：比XA的程序运行的快340ms，在2018年到来之前完工//以后的算法估计都可以在这上面动刀子了
         //2017/11/9新建//2017/11/21/Version 1.0//2017/11/23/Version 1.1//11/25/Version 1.2
         public static void Main(string[] args)
         {
-            //StringBuilder sensitiveWordSb=new StringBuilder();
-            using (StreamReader streamReader = new StreamReader(@"E:\BaiduNetdiskDownload\SensitiveWords.txt"))
-            {
-                List<string> sortedWordList = new List<string>();
-                var tree = new Tree();
-                while (!streamReader.EndOfStream)
-                {
-                    var currentWord = streamReader.ReadLine();
-                    if (currentWord != null)
-                    {
-                        sortedWordList.Add(currentWord);
-                    }
-                }
-                SortedMethods.QuickSort(ref sortedWordList, 0, sortedWordList.Count);
-                foreach (var word in sortedWordList)
-                {
-                    tree.PlantATree(word[0], word);
-                }
-                tree.SearchTree("免定金雄鹰AWP");
-                Console.ReadKey();
-            }
+            //using (StreamReader streamReader = new StreamReader(@"E:\BaiduNetdiskDownload\SensitiveWords.txt"))
+            //{
+            //    List<string> sortedWordList = new List<string>();
+            //    var tree = new Tree();
+            //    while (!streamReader.EndOfStream)
+            //    {
+            //        var currentWord = streamReader.ReadLine();
+            //        if (currentWord != null)
+            //        {
+            //            sortedWordList.Add(currentWord);
+            //        }
+            //    }
+            //    SortedMethods.QuickSort(ref sortedWordList, 0, sortedWordList.Count);
+            //    foreach (var word in sortedWordList)
+            //    {
+            //        tree.PlantATree(word[0], word);
+            //    }
+            //    tree.SearchTree("免定金雄鹰AWP");
+            //    Console.ReadKey();
+            //}
         }
     }
 }
